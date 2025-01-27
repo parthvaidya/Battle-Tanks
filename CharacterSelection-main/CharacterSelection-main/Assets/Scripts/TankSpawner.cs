5ea4336fc0eb18c41cfcc6def04865cb740b16af
Parth Vaidya <zeusparth99@gmail.com>
2025-01-27 18:46:01 +0530

    Creation of tank
    Added  Prefab of the tank.
    Spawn the tank prefab in Scene.

---
+++ CharacterSelection-main/CharacterSelection-main/Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject tankPrefab;
+
+    void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+    }
+}
