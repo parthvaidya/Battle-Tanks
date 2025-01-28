80a3253d751dba2f22b724c5917d5f9a46e753a5
Parth Vaidya <zeusparth99@gmail.com>
2025-01-28 15:15:06 +0530

    MVC Added

---
+++ CharacterSelection-main/CharacterSelection-main/Assets/Scripts/TankModel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankModel 
+{
+    private TankController tankController;
+
+    public TankModel()
+    {
+
+    }
+
+    public void SetTankController(TankController _tankController)
+    {
+        tankController = _tankController;
+    }
+}
--- CharacterSelection-main/CharacterSelection-main/Assets/Scripts/TankSpawner.cs
+++ CharacterSelection-main/CharacterSelection-main/Assets/Scripts/TankSpawner.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
-    public GameObject tankPrefab;
+    public TankView tankView;
 
     void Start()
     {
-        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+        CreateTank();
+    }
+
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankModel, tankView);
     }
 }
